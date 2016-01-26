9f8685db0e0c12fa02439f580b885fe1160b1a3f
William <william.kapke@gmail.com>
2016-01-26 02:02:48 +0000

    a more accurate ObjectId compare

--- sift.js
+++ sift.js
@@ -4,11 +4,12 @@ var sift = require('sift');
 var compare = sift.compare;
 sift.compare = function(a, b) {
   if(a && b && a._bsontype && b._bsontype) {
-    if(a.equals(b)) return 0;
-    a = a.getTimestamp().getTime();
-    b = b.getTimestamp().getTime();
+    return a.equals(b)? 0 : (compare(time(a), time(b)) || compare(a.str, b.str));
   }
   return compare(a,b);
 };
+function time(id) {
+  return id.getTimestamp().getTime()
+}
 
 module.exports = sift;
